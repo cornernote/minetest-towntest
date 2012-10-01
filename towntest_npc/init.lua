f0461f6c0973a2c4de405431ba256919f26dfb7f
Brett O'Donnell <cornernote@gmail.com>
2012-10-01 11:36:53 +0930

    fix 3d player

--- towntest_npc/init.lua
+++ towntest_npc/init.lua
@@ -15,39 +15,37 @@ if minetest.setting_getbool("3d_models") then
 	function x(val) return ((val -80) / 160) end
 	function z(val) return ((val -80) / 160) end
 	function y(val) return ((val + 80) / 160) end
-	local textures = {
-		"towntest_npc_builder_bottom.png",
-		"towntest_npc_builder_top.png",
-		"towntest_npc_builder_back.png",
-		"towntest_npc_builder_front.png",
-		"towntest_npc_builder_right.png",
-		"towntest_npc_builder_left.png",
-	}
-	local nodebox ={
-		--head
-		{x(95),y(-10), z(65), x(65), y(-40), z(95)},
-		--neck
-		{x(90),y(-40),z(70) , x(70), y(-50),z(90) },
-		--body
-		{x(90),y(-50), z(60), x(70), y(-100), z(100)},
-		--legs
-		{x(90),y(-100), z(60),x(70), y(-160),z(79) },
-		{x(90),y(-100), z(81),x(70), y(-160), z(100)},
-		--shoulders
-		{x(89),y(-50), z(58), x(71),y(-68),z(60)},
-		{x(89),y(-50), z(100),x(71) ,y(-68),z(102)},
-		--left arm
-		{x(139),y(-50),z(45),x(71),y(-63),z(58)},
-		--right arm
-		{x(89),y(-50),z(102),x(71),y(-100),z(115)},
-		{x(115),y(-87),z(102),x(71),y(-100),z(115)},
-	}
 	minetest.register_node("towntest_npc:builder_box", {
-		tiles = textures,
+		tiles = {
+			"towntest_npc_builder_bottom.png",
+			"towntest_npc_builder_top.png",
+			"towntest_npc_builder_back.png",
+			"towntest_npc_builder_front.png",
+			"towntest_npc_builder_right.png",
+			"towntest_npc_builder_left.png",
+		},
 		drawtype = "nodebox",
 		node_box = {
 			type = "fixed",
-			fixed = nodebox
+			fixed = {
+				--head
+				{x(95),y(-10), z(65), x(65), y(-40), z(95)},
+				--neck
+				{x(90),y(-40),z(70) , x(70), y(-50),z(90) },
+				--body
+				{x(90),y(-50), z(60), x(70), y(-100), z(100)},
+				--legs
+				{x(90),y(-100), z(60),x(70), y(-160),z(79) },
+				{x(90),y(-100), z(81),x(70), y(-160), z(100)},
+				--shoulders
+				{x(89),y(-50), z(58), x(71),y(-68),z(60)},
+				{x(89),y(-50), z(100),x(71) ,y(-68),z(102)},
+				--left arm
+				{x(139),y(-50),z(45),x(71),y(-63),z(58)},
+				--right arm
+				{x(89),y(-50),z(102),x(71),y(-100),z(115)},
+				{x(115),y(-87),z(102),x(71),y(-100),z(115)},
+			}
 		},
 	})
 	visual = "wielditem"
