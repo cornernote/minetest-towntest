b97cc33522cc5d098ea0bfe673888b552b658dbb
Brett O'Donnell <cornernote@gmail.com>
2012-10-13 11:33:10 +1030

    remove random game_id, not needed

--- towntest/init.lua
+++ towntest/init.lua
@@ -11,8 +11,5 @@ License: GPLv3
 -- expose api
 towntest = {}
 
--- unique game id, needed to cleanup npcs
-towntest.game_id = math.random(100000000000)
-
 -- log that we started
 minetest.log("action", "[MOD]"..minetest.get_current_modname().." -- loaded from "..minetest.get_modpath(minetest.get_current_modname()))
